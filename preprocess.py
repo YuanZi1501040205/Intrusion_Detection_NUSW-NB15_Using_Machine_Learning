08d9b539459738e0cc930a7aaa49f53247ed6150
yzi2 <yzi2@central.uh.edu>
2020-03-12 23:22:21 -0500

    train

--- preprocess.py
+++ preprocess.py
@@ -1,131 +1,142 @@
-"""cv_hw1.py: Starter file to run howework 2"""
-
-#Example Usage: ./cv_hw1 -i image -k clusters -m grey
-#Example Usage: python cv_hw1 -i image -k clusters -m rgb
+"""preprocess.py: tool file to prepare UNSW_NB15 dataset to train"""
 
 
-__author__      = "Pranav Mantini"
-__email__ = "pmantini@uh.edu"
+#Example Usage: python preprocess.py
+
+
+__author__      = "Yuan Zi"
+__email__ = "yzi2@central.uh.edu"
 __version__ = "1.0.0"
 
-import cv2
-import sys
-from Segmentation.KMeans import KmeansSegmentation
-from datetime import datetime
-##%%
-# display_image("image","fruits.jpg")
-# # %%
-# def IMGstatistics(image_path, color, output_path):
-#     import matplotlib.pyplot as plt
-#     if color == 'GRAY':
-#         hist = np.zeros([256], dtype = np.int32)
-#         image = cv2.imread(image_path, 0)
-#         height, width = image.shape[0], image.shape[1]
-#         if image is None:
-#             print("image reading failure, please check image's path!")
-#             exit()
-#         for row in range(height):
-#             for col in range(width):
-#                 pv = image[row, col]
-#                 hist[pv] +=1
-#         plt.figure()
-#         plt.title(image_path + " Gray Histogram")
-#         plt.xlabel("intensity")
-#         plt.ylabel("number of pixels")
-#         # plt.hist(hist, bins=8000, density = 'TRUE', facecolor = 'green', alpha = 0.5)
-#         plt.plot(hist, color="r")
-#         plt.xlim([0,256])
-#         plt.savefig(output_path + "GRAY_statictics_" + image_path)
-#         plt.show()
-#     elif color == 'RGB':
-#         image = cv2.imread(image_path)
-#         height, width = image.shape[0], image.shape[1]
-#         if image is None:
-#             print("image reading failure, please check image's path!")
-#             exit()
-#         histb = np.zeros([256], dtype = np.int32)
-#         histg = np.zeros([256], dtype = np.int32)
-#         histr = np.zeros([256], dtype = np.int32)
-#         for row in range(height):
-#             for col in range(width):
-#                 pvb = image[row, col, 0]
-#                 histb[pvb] +=1
-#                 pvg = image[row, col, 1]
-#                 histg[pvg] += 1
-#                 pvr = image[row, col, 2]
-#                 histr[pvr] += 1
-#         plt.figure()
-#         plt.title(image_path + " RGB Histogram")
-#         plt.xlabel("intensity")
-#         plt.ylabel("number of pixels")
-#         plt.plot(histb, color = 'b')
-#         plt.plot(histg, color = 'g')
-#         plt.plot(histr, color = 'r')
-#         plt.xlim([0,256])
-#         plt.savefig(output_path + "RGB_statictics_" + image_path)
-#         plt.show()
-#     else :
-#         print("input type error!")
-#         pass
-# # %%
-# image_path = 'circles.jpg'
-# color = 'RGB'
-# output_path = '/home/cougarnet.uh.edu/yzi2/Pictures/'
-# IMGstatistics(image_path, color, output_path)
-#
-# # %%
-def main():
-    """ The main funtion that parses input arguments, calls the approrpiate
-     kmeans method and writes the output image"""
 
-    #Parse input arguments
-    from argparse import ArgumentParser
+# Import APIs
+import pandas as pd
+import numpy as np
 
-    parser = ArgumentParser()
 
-    parser.add_argument("-i", "--image", dest="image",
-                        help="specify the name of the image", metavar="IMAGE")
-    parser.add_argument("-k", "--clusters", dest="clusters",
-                        help="Specify the number of clusters (k)", metavar="CLUSTERS")
-    parser.add_argument("-m", "--model", dest="model",
-                        help="Specify the model rgb, grey", metavar="COLOR")
 
-    args = parser.parse_args()
+def merge4datasets(path1, path2, path3, path4):
 
-    #Load image
-    if args.image is None:
-        print("Please specify the name of image")
-        print("use the -h option to see usage information")
-        sys.exit(2)
+    """ The merge4datasets funtion prepare dataset for
+     two classifier take 4 dataset input path add head and merge them to one."""
+    import pandas as pd
+    # load dataset
+    column_names = ["srcip", "sport", "dstip", "dsport", "proto", "state", "dur", "sbytes", "dbytes", "sttl", "dttl", "sloss", "dloss", "service", "sload", "dload", "spkts", "dpkts", "swin", "dwin", "stcpb", "dtcpb", "smean", "dmean", "trans_depth", "response_body_len", "sjit", "djit", "stime", "ltime", "sinpkt", "dinpkt", "tcprtt", "synack", "ackdat", "is_sm_ips_ports", "ct_state_ttl", "ct_flw_http_mthd", "is_ftp_login", "ct_ftp_cmd", "ct_srv_src", "ct_srv_dst", "ct_dst_ltm", "ct_src_ltm", "ct_src_dport_ltm", "ct_dst_sport_ltm", "ct_dst_src_ltm", "attack_cat", "label"]
+    dataset1 = pd.read_csv(path1)
+    dataset2 = pd.read_csv(path2)
+    dataset3 = pd.read_csv(path3)
+    dataset4 = pd.read_csv(path4)
+    dataset1.columns = column_names
+    dataset2.columns = column_names
+    dataset3.columns = column_names
+    dataset4.columns = column_names
+    #merge
+    dataset = dataset1.append(dataset2, ignore_index=True)
+    dataset = dataset.append(dataset3, ignore_index=True)
+    dataset = dataset.append(dataset4, ignore_index=True)
+    for row in range(len(dataset['label'])):
+        if dataset.at[row, 'label'] == 0:
+            dataset.at[row, 'attack_cat'] = 'Normal'
+    return dataset
+
+# mapping
+
+def mapping(dataset):
+    """ delete unrelated feature id and attacks' category label and Mapping nominal features(proto, state, service, attack_cat) to int"""
+    import numpy as np
+
+    # delete unrelated feature id and attacks' category label
+    after_drop_dataset = dataset.drop(columns=['srcip', 'dstip', 'ct_flw_http_mthd', 'is_ftp_login', 'stime', 'ltime'])
+    after_drop_dataset.isna().sum()
+    ## Mapping nominal features(proto, state, service, attack_cat) to int
+
+    # Mapping categories data
+    proto_mapping = {label: idx for idx, label in enumerate(np.unique(after_drop_dataset['proto']))}
+    state_mapping = {label: idx for idx, label in enumerate(np.unique(after_drop_dataset['state']))}
+    service_mapping = {label: idx for idx, label in enumerate(np.unique(after_drop_dataset['service']))}
+    attack_cat_mapping = {label: idx for idx, label in enumerate(np.unique(after_drop_dataset['attack_cat']))}
+    after_drop_dataset['proto'] = after_drop_dataset['proto'].map(proto_mapping)
+    after_drop_dataset['state'] = after_drop_dataset['state'].map(state_mapping)
+    after_drop_dataset['service'] = after_drop_dataset['service'].map(service_mapping)
+    after_drop_dataset['attack_cat'] = after_drop_dataset['attack_cat'].map(attack_cat_mapping)
+
+    ## dealling with missing values(delete all cause low proportion)
+    # convert inf to Nan then delete sample containing NaN
+    after_drop_dataset.replace([np.inf, -np.inf], np.nan)
+    after_drop_dataset = after_drop_dataset.dropna()
+
+    # remapping back the attack categories
+    inv_attack_cat_mapping = {v: k for k, v in attack_cat_mapping.items()}
+    # "debug" inverse mapping to show original feature
+    # inv_proto_mapping = {v: k for k, v in proto_mapping.items()}
+    # train_dataset['proto'] = train_dataset['proto'].map(inv_proto_mapping)
+    # print(proto_mapping)
+    return(after_drop_dataset, inv_attack_cat_mapping)
+
+def normalization(dataset, normtype):
+
+    """Normalize each column based on max-min normalization or standard scalar"""
+    from sklearn import preprocessing
+    import pandas as pd
+    # 1.convert the whole dataframe value of the dataframe as floats, creating NaNs when necessary
+    dataset = dataset.apply(pd.to_numeric, args=('coerce',))
+
+    # convert inf to Nan then delete sample containing NaN
+    dataset.replace([np.inf, -np.inf], np.nan)
+    dataset = dataset.dropna()
+
+    if normtype == "maxmin":
+       # normalize train features with maxmin function
+       for feature_name in dataset.columns[:-1]:
+           # 1.convert the column value of the dataframe as floats
+           col_array = dataset[feature_name].values.astype(float)
+           col_array = col_array.reshape(-1, 1)
+           # 2. create a min max processing object
+           min_max_scaler = preprocessing.MinMaxScaler()
+           scaled_array = min_max_scaler.fit_transform(col_array)
+           dataset[feature_name] = pd.DataFrame(scaled_array)
+    elif normtype == "std":
+       # normalize train features with standard function
+       for feature_name in dataset.columns[:-1]:
+           col_array = dataset[feature_name].values.astype(float)
+           col_array = col_array.reshape(-1, 1)
+           # 2. create a std processing object
+           std_scaler = preprocessing.StandardScaler()
+           scaled_array = std_scaler.fit_transform(col_array)
+           dataset[feature_name] = pd.DataFrame(scaled_array)
     else:
-        image_name = args.image.split(".")[0]
-        input_image = cv2.imread(args.image)
+        print("normalize failed, please check your data!")
+        pass
+
+    # delete sample containing NaN, These NaN may appear because divided 0 or have inf number
+    dataset = dataset.dropna()
+    # # %% count how man NaN(debug)
+    # for feature_name in X_test.columns:
+    #     count = X_test[feature_name].isna().sum()
+    #     print(count)
+    return(dataset)
 
 
-    if args.clusters is None:
-        print("Number of clusters not specified using 2")
-        print("use the -h option to see usage information")
-        clusters = 2
-    else:
-        clusters = int(args.clusters)
+def split(dataset, rate, classifier):
+    """Choose two or Multi classes classifier and split dataset with assigned rate"""
+    from sklearn.model_selection import train_test_split
+    if classifier == 'two':
+        X_train, X_test, Y_train, Y_test = train_test_split(dataset.drop(['label', 'attack_cat'], axis=1), dataset.label, test_size=rate, random_state=0)
+    elif classifier == 'multi':
+        X_train, X_test, Y_train, Y_test = train_test_split(dataset.drop(['label', 'attack_cat'], axis=1), dataset.attack_cat, test_size=rate, random_state=0)
+    return(X_train, X_test, Y_train, Y_test)
 
-    # Check resize scale parametes
-    if args.model is None:
-        print("Model not specified using default (grey)")
-        print("use the -h option to see usage information")
-        model = 'grey'
-    elif args.model not in ['rgb', 'grey']:
-        print("Unknown color model, using default (grey)")
-        print("use the -h option to see usage information")
-        model = 'grey'
-    else:
-        model = args.model
 
-    Segementation_object = KmeansSegmentation()
 
-    output = None
-    if model == 'grey':
-        input_image = cv2.cvtColor(input_image, cv2.COLOR_RGB2GRAY)
-        output = Segementation_object.segmentation_grey(input_image, clusters, image_name)
-    else:
-        output = Segementation_object.segmentation_rgb(input_image, clusters, image_name)
+def pca_analysis(X_train, X_test, n_features, after_norm_dataset):
+    """ The funtion that analysis importance features with PCA"""
+    from sklearn.decomposition import PCA
+    pca = PCA(n_components = n_features)
+    X_train = pca.fit_transform(X_train)
+    X_test = pca.transform(X_test)
+    explained_variance = pca.explained_variance_ratio_
+    i = 0
+    for col in after_norm_dataset.columns[:-2]:
+        print(col+' ', explained_variance[i])
+        i = i + 1
+    return(X_train, X_test)
